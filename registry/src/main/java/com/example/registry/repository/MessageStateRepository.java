782fac0bbda0f58df5a1af2f6d069f7982f606b5
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-05-08 18:16:42 +0500

    remove message state

--- registry/src/main/java/com/example/registry/repository/MessageStateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.registry.repository;
-
-import com.example.registry.model.MessageState;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageStateRepository extends JpaRepository<MessageState, Long> {
-}
