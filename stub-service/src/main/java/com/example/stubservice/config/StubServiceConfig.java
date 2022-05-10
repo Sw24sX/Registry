2d1b5f3708bb2ee4818855fd8a1aaffa5f630189
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-05-10 14:50:20 +0500

    stub service refactor

--- stub-service/src/main/java/com/example/stubservice/config/StubServiceConfig.java
+++ stub-service/src/main/java/com/example/stubservice/config/StubServiceConfig.java
@@ -1,15 +1,12 @@
 package com.example.stubservice.config;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
 
 @Configuration
 @EnableJms
@@ -23,16 +20,6 @@ public class StubServiceConfig {
     @Value("${activemq.password}")
     private String password;
 
-//    @Bean
-//    public Destination addDestination() {
-//        return new ActiveMQQueue("add-user-queue");
-//    }
-//
-//    @Bean
-//    public Destination getRequestDestination() {
-//        return new ActiveMQQueue("get-user-queue");
-//    }
-
     @Bean
     public ConnectionFactory connectionFactory() {
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
@@ -42,8 +29,4 @@ public class StubServiceConfig {
         return factory;
     }
 
-//    @Bean
-//    public JmsTemplate jmsTemplate(){
-//        return new JmsTemplate(connectionFactory());
-//    }
 }
--- stub-service/src/main/java/com/example/stubservice/service/StubService.java
+++ stub-service/src/main/java/com/example/stubservice/service/StubService.java
@@ -7,7 +7,6 @@ import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.jms.*;
